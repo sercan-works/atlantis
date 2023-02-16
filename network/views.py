e3cd762be3927081406e05489a2be1c030fcc669
sercan-works <sercantas1026@gmail.com>
2023-02-16 18:17:42 +0300

    şı7r

--- network/views.py
+++ network/views.py
@@ -3,10 +3,15 @@ from django.shortcuts import render
 # from network.ping_ip import ping_ip
 import ping3
 
-       
-
+IP_ADDRESS = {
+    "lol":'185.40.65.1'
+}
+data = {
+    "lol":0,
+    "valorant":0,
+}# initial values yazılacak bu değeler sayfaya gönderielcek:
 def ping_page(request):
     ip_address = '104.160.143.212'  
-    response = ping3.ping(ip_address, unit='ms') *1
+    response = ping3.ping(ip_address, unit='ms') * 1
     return render(request, 'network/index.html', {'response': int(response)})
     
